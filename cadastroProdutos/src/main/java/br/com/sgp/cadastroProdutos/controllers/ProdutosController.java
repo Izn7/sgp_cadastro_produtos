a6b1c5fe11627922537099e5c923ef9a963f999c
Acesso Livre <Acesso Livre@BC7N278834>
2026-06-11 21:23:22 -0300

    Cadastro peças

---
+++ cadastroProdutos/src/main/java/br/com/sgp/cadastroProdutos/controllers/ProdutosController.java
@@ -0,0 +1,73 @@
+package br.com.sgp.cadastroProdutos.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import br.com.sgp.cadastroProdutos.entity.ProdutosEntity;
+import br.com.sgp.cadastroProdutos.repository.ProdutosRepository;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutosController {
+
+		@Autowired
+		public ProdutosRepository produtosRepository;
+		
+				//acharId
+				@GetMapping("/{id}")
+				@ResponseStatus(HttpStatus.OK)
+				public Optional<ProdutosEntity> buscarId(@PathVariable Long id){
+					return produtosRepository.findById(id);
+				}
+				
+				//acharTodos
+				@GetMapping("/acharTodos")
+				@ResponseStatus(HttpStatus.OK)
+				public List<ProdutosEntity> buscarTodos(){
+					return produtosRepository.findAll();
+				}
+				
+				//gravar
+				@PostMapping("/gravar")
+				@ResponseStatus(HttpStatus.CREATED)
+				public ProdutosEntity Gravar(@RequestBody ProdutosEntity produtosEntity) {
+					return produtosRepository.save(produtosEntity);
+					
+				}
+				
+				
+				//atualizar
+				@PutMapping("/atualizar/{id}")
+				@ResponseStatus(HttpStatus.OK)
+				public ProdutosEntity atualizar(@RequestBody ProdutosEntity produtosEntity, @PathVariable Long id) {
+					produtosEntity.setId(id);
+					return produtosRepository.save(produtosEntity);
+				}
+				
+				//deletar
+				@DeleteMapping("/deletar/{id}")
+				@ResponseStatus(HttpStatus.OK)
+				public String Deletar(@PathVariable Long id) {
+					if(produtosRepository.existsById(id)) {
+						produtosRepository.deleteById(id);
+						return "Deletado";
+					}
+						return "Não Deletado";
+					}
+
+
+	
+}
---
+++ cadastroProdutos/src/main/resources/static/js/CadastroProdutos.js
@@ -0,0 +1,145 @@
+// Chamando API
+const API_BUSCAR_TODOS = 'http://localhost:8012/produtos/acharTodos';
+const API_SALVAR = 'http://localhost:8012/produtos/gravar';
+const API_BUSCAR_ID = 'http://localhost:8012/produtos';
+const API_DELETAR = 'http://localhost:8012/produtos/deletar';
+const API_ATUALIZAR = 'http://localhost:8012/produtos/atualizar';
+
+// Variável de controle
+let editandoId = null;
+
+// Buscar todos os produtos
+async function buscarProdutos() {
+
+    const response = await fetch(API_BUSCAR_TODOS, {
+        method: 'GET'
+    });
+
+    const produtos = await response.json();
+
+    const tbody = document.getElementById('tabelaProdutos');
+    tbody.innerHTML = "";
+
+    produtos.forEach(produto => {
+
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+            <td>${produto.sku}</td>
+            <td>${produto.descricao}</td>
+            <td>${produto.marca}</td>
+            <td>${produto.precoCusto}</td>
+            <td>${produto.precoVenda}</td>
+            <td>${produto.peso}</td>
+            <td>${produto.estoqueMin}</td>
+            <td>${produto.estoqueMax}</td>
+            <td>${produto.ativo}</td>
+            <td>
+                <button class="btn btn-warning btn-sm"
+                    onclick="editar(${produto.id})">
+                    Editar
+                </button>
+
+                <button class="btn btn-danger btn-sm"
+                    onclick="deletar(${produto.id})">
+                    Deletar
+                </button>
+            </td>
+        `;
+
+        tbody.appendChild(tr);
+
+    });
+
+}
+
+// Cadastrar ou atualizar
+async function cadastrarProdutos() {
+
+    const produto = {
+
+        sku: document.getElementById('sku').value,
+        descricao: document.getElementById('descricao').value,
+        categoria: document.getElementById('categoria').value,
+        marca: document.getElementById('marca').value,
+        precoCusto: parseFloat(document.getElementById('precoCusto').value),
+        precoVenda: parseFloat(document.getElementById('precoVenda').value),
+        peso: parseFloat(document.getElementById('peso').value),
+        estoqueMin: parseInt(document.getElementById('estoqueMin').value),
+        estoqueMax: parseInt(document.getElementById('estoqueMax').value),
+        ativo: document.getElementById('ativo').checked
+    };
+
+   
+
+        if (editandoId) {
+
+            response = await fetch(`${API_ATUALIZAR}/${editandoId}`, {
+
+                method: 'PUT',
+
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+
+                body: JSON.stringify(produto)
+
+            });
+
+        } else {
+
+            response = await fetch(API_SALVAR, {
+
+                method: 'POST',
+
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+
+                body: JSON.stringify(produto)
+
+            });
+
+        }
+
+     
+
+    }
+
+
+
+
+// Deletar
+async function deletar(id) {
+
+    if (!confirm("Deseja excluir?")) return;
+
+    await fetch(`${API_DELETAR}/${id}`, {
+        method: 'DELETE'
+    });
+
+    buscarProdutos();
+
+}
+
+// Editar
+async function editar(id) {
+
+    const response = await fetch(`${API_BUSCAR_ID}/${id}`);
+    const produto = await response.json();
+
+    editandoId = id;
+
+    document.getElementById("sku").value = produto.sku;
+    document.getElementById("descricao").value = produto.descricao;
+    document.getElementById("categoria").value = produto.categoria;
+    document.getElementById("marca").value = produto.marca;
+    document.getElementById("precoCusto").value = produto.precoCusto;
+    document.getElementById("precoVenda").value = produto.precoVenda;
+    document.getElementById("peso").value = produto.peso;
+    document.getElementById("estoqueMin").value = produto.estoqueMin;
+    document.getElementById("estoqueMax").value = produto.estoqueMax;
+    document.getElementById("ativo").checked = produto.ativo;
+
+}
+
