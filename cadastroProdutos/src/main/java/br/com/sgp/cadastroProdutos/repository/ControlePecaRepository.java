9ec07e28f7a649e80d7786f1660e1d497c6cd92a
Acesso Livre <Acesso Livre@BC7N278834>
2026-06-17 22:17:59 -0300

    Versão final v2

--- cadastroProdutos/src/main/java/br/com/sgp/cadastroProdutos/repository/ControlePecaRepository.java
+++ cadastroProdutos/src/main/java/br/com/sgp/cadastroProdutos/repository/ControlePecaRepository.java
@@ -3,9 +3,10 @@ package br.com.sgp.cadastroProdutos.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.sgp.cadastroProdutos.entity.ControlePecaEntity;
+import br.com.sgp.cadastroProdutos.entity.ControleEstoquePecasEntity;
+
 
 @Repository
-public interface ControlePecaRepository extends JpaRepository<ControlePecaEntity, Integer> {
+public interface ControlePecaRepository extends JpaRepository<ControleEstoquePecasEntity, Integer> {
 
 }
